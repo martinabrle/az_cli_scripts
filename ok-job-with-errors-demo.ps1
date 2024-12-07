cf2f2ba28dab3d334936f30038c507d5a7bd9b59
Martin Abrle <71389217+martinabrle@users.noreply.github.com>
2024-12-07 13:49:58 +0100

    Update ok-job-with-errors-demo.ps1

--- ok-job-with-errors-demo.ps1
+++ ok-job-with-errors-demo.ps1
@@ -1,18 +1,18 @@
 # KQL:
-# // Needs to redesign to only return completed jobs
+# // Selecting all job stream errors and joining those with jobs which have a status completed
 # AzureDiagnostics 
 # | where ResourceProvider == "MICROSOFT.AUTOMATION"
 #     and Category == "JobStreams"
-#     //and ResultType == "Completed"  
 #     and StreamType_s == "Error" 
+# | join kind=inner (
+#     AzureDiagnostics
+#     | where ResultType == "Completed"
+#     | project-rename ParentJobId_g=JobId_g, ParentResultType=ResultType
+#     | distinct ParentJobId_g, ParentResultType
+#     )
+#     on $left.JobId_g == $right.ParentJobId_g
 # | project
-#     TimeGenerated,
-#     RunbookName_s,
-#     StreamType_s,
-#     _ResourceId,
-#     ResultDescription,
-#     JobId_g,
-#     ResultType
+#     TimeGenerated, RunbookName_s, StreamType_s, ResultDescription, ParentJobId_g, ResultType, ParentResultType, _ResourceId, id_s
 
 # Ensures you do not inherit an AzContext in your runbook
 Disable-AzContextAutosave -Scope Process
